5a6effe20634c23b4f45e7be2fd5e126f6e2293f
zhangsan <zhangsan@.com>
2023-01-14 11:34:39 +0800

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-export function exportXlsx(data,cloumData,xlsxName){
+export function exportXlsx(data,cloumData,xlsxName){    
     let execlContent='';
     let contentHead=Object.keys(cloumData)
     for(let i=0;i<contentHead.length;i++){
@@ -24,10 +24,13 @@ export function exportXlsx(data,cloumData,xlsxName){
     }
 
     execlContent.trim()
-
+    //我们将拼接好的字符串声明一个blob对象，记得将我们的字符串放到第一个数组参数中，然后我们在传入第二个指定文本格式的对象参数
     let blob=new Blob([execlContent],{ type:"text/plain;charset=utf-8"})
+    //为了防止中文乱码我们在使用String.fromCharCode(0xFEFF)结合我们刚才声明的blob对象再次声明成新的blob对象
     blob=new Blob([String.fromCharCode(0xFEFF),blob],{type:blob.type})
+    //然后我们使用URL的createObjectUrl将blob示例变成链接对象
     const execlUrl=window.URL.createObjectURL(blob)
+    //最后创建一个a标签然后将其href指定为我们生成的链接对象，然后配置down属性为我们下载的文件名，最后触发单机事件即可
     const link=document.createElement('a')
     link.href=execlUrl
     link.download=`${xlsxName}.xls`
